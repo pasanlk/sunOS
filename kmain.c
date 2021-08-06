cf0ea9d03a22ed1dbebb079500559a405a03353b
Pasan lahiru dissanayake <70741538+pasanlk@users.noreply.github.com>
2021-08-06 09:23:34 +0530

    Play with `Outputs`

--- kmain.c
+++ kmain.c
@@ -1,5 +1,14 @@
-    /* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
-    {
-        return arg1 + arg2 + arg3;
+#include "frame_buffer.h"
+#include "serial_port.h"
+
+
+    
+    int main(){
+
+           char ptr2[] = "Welcome Pasan";
+   
+    
+    serial_write(0x3F8, ptr2, 14);
+    fb_write(ptr2, 14);
+    
     }
